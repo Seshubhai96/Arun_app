86afa2fc2b7d2108b26ee303016d0320963cb554
sheshasai <sheshasai.k@lionorbit.com>
2024-11-28 10:26:05 +0530

    api setup

--- lib/api/apimethods.dart
+++ lib/api/apimethods.dart
@@ -7,26 +7,14 @@ class Api extends GetConnect{
   Future<dynamic>? loginmethod({body,endpoint})async{
     try {
       final url = "$baseurl$endpoint";
-      // log(url.toString());
-      // log(body.toString());
       final response =  await post(url,body,headers: headers);
-      //log(response.body.toString());
-      // log(response.headers.toString());
-      // log(response.isOk.toString());
-      // log(response.statusText.toString());
       if(response.isOk){
-        //log("sucess");
-        //log("Actual Response ${response.body}");
        final extractres = response.body;
-       //log("Respinse Api $extractres");
       return extractres?["data"];
       }
-      else{
        return null;
-      }
-     
     } catch (e) {
-      log("Api Error ${e.toString()}");
+      //log("Api Error ${e.toString()}");
       return null;
     }
   }
